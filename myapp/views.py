988a37e8265699e7d2b9450c3784b3d1916aa241
AngelOrdon02 <3006613200101@ingenieria.usac.edu.gt>
2022-10-16 04:08:46 -0700

    Show users - my API

--- myapp/views.py
+++ myapp/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
+import json
+
 # Create your views here.
 def users(request):
 
     #pull data from third party rest api
-    response = requests.get('https://jsonplaceholder.typicode.com/users')
+    # response = requests.get('https://jsonplaceholder.typicode.com/users')
+    response = requests.get('http://127.0.0.1:3000/users')
 
     #convert reponse data into json
     users = response.json()
@@ -14,5 +17,6 @@ def users(request):
     
     # return HttpResponse("Users")
     # return render(request, "users.html")
-    return render(request, "users.html", {'users': users})
+    # return render(request, "users.html", {'users': users})
+    return render(request, "users.html", {'users': users['users']})
     pass
